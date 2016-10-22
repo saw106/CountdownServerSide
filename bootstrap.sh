3186e3f5ad0db6b70050c09cbe47b77d9c10008d
Steven Wendling <saw106@case.edu>
2016-10-22 16:22:29 -0400

    added README and updates bootstrap to reflect using sqlite3

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,3 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install python-pip
 pip install flask
-apt-get install postgresql-9.3
-# apt-get install postgresql-client
-# TODO: setup psql server
