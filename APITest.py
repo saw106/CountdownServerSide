38d0507a7659ad877a37d0fa1a21e2a0efeaec87
Steven Wendling <saw106@case.edu>
2016-11-20 13:35:41 -0500

    switched all assertions to use ids instead of longer strings

--- APITest.py
+++ APITest.py
@@ -57,34 +57,32 @@ class APITest(unittest.TestCase):
 
     def test_06_SeeActiveTasks(self):
         r = requests.get('http://localhost:5000/get_active_tasks', headers=joe_headers)
-        print r.content
         # I had to leave out the datetime values because those will be different
         expected1 = (""""tasks": [{"description": "this is a task and it will be done","""
                      """ "backgroundhex": "#000000",""")
         expected2 = (""" "id": 0,""")
 
-        # expected3 = (""" "foregroundhex": "#000000","""
-        #              """ "subtaskof": null,"""
-        #              """ "duedate": "10-1-2016T00:00Z","""
-        #              """ "name": "good task"},""")
-        # expected4 = ("""{"description": "this is a task and it will be done","""
-        #              """ "backgroundhex": "#000000",""")
-        # expected5 = (""" "completed": false,"""
-        #              """ "id": 1,"""
-        #              """ "priority": "omega","""
-        #              """ "tag": "things I hate to do","""
-        #              """ "completiontime": null,""")
-        # expected6 = (""" "foregroundhex": "#000000","""
-        #              """ "subtaskof": null,"""
-        #              """ "duedate": "10-2-2016T00:00Z","""
-        #              """ "name": "good task"}]}""")
+        expected3 = (""" "foregroundhex": "#000000","""
+                     """ "subtaskof": null,"""
+                     """ "duedate": "10-1-2016T00:00Z","""
+                     """ "name": "good task"},""")
+        expected4 = ("""{"description": "this is a task and it will be done","""
+                     """ "backgroundhex": "#000000",""")
+        expected5 = (""" "completed": false,"""
+                     """ "id": 1,"""
+                     """ "priority": "omega","""
+                     """ "tag": "things I hate to do","""
+                     """ "completiontime": null,""")
+        expected6 = (""" "foregroundhex": "#000000","""
+                     """ "subtaskof": null,"""
+                     """ "duedate": "10-2-2016T00:00Z","""
+                     """ "name": "good task"}]}""")
         self.assertTrue(expected1 in r.content)
-        # print (expected1 in r.content)
         self.assertTrue(expected2 in r.content)
-        # self.assertTrue(expected3 in r.content)
-        # self.assertTrue(expected4 in r.content)
-        # self.assertTrue(expected5 in r.content)
-        # self.assertTrue(expected6 in r.content)
+        self.assertTrue(expected3 in r.content)
+        self.assertTrue(expected4 in r.content)
+        self.assertTrue(expected5 in r.content)
+        self.assertTrue(expected6 in r.content)
 
     def test_07_SeeNextEndingTask(self):
         r = requests.get('http://localhost:5000/get_next_countdown', headers=joe_headers)
@@ -128,17 +126,13 @@ class APITest(unittest.TestCase):
         r = requests.get('http://localhost:5000/get_subtasks/0', headers=joe_headers)
         expected1 = (""""tasks": [{"description": "I DO NOT BELONG WITH NORMAL TASKS","""
                      """ "backgroundhex": "#000000",""")
-        expected2 = (""" "completed": "true","""
-                     """ "id": 2,"""
-                     """ "priority": "omega","""
-                     """ "tag": "things I hate to do","""
-                     """ "completiontime": null,""")
+        expected2 = (""" "id": 2,""")
         expected3 = (""" "foregroundhex": "#000000","""
                      """ "subtaskof": 0,"""
                      """ "duedate": "10-2-2016T00:00Z","""
                      """ "name": "I AM A SUBTASK"}]}""")
         self.assertTrue(expected1 in r.content)
-        # self.assertTrue(expected2 in r.content)
+        self.assertTrue(expected2 in r.content)
         self.assertTrue(expected3 in r.content)
 
     def test_11_CompleteTask1(self):
@@ -178,20 +172,15 @@ class APITest(unittest.TestCase):
         self.assertEquals("true", get_status_result(r.content))
         # Verify the updates
         r = requests.get('http://localhost:5000/get_active_tasks', headers=joe_headers)
-        # print r.content
         expected1 = ("""{"description": "this is a task and it has been modified","""
                      """ "backgroundhex": "#1111",""")
-        expected2 = (""" "completed": false,"""
-                     """ "id": '1',"""
-                     """ "priority": "swag","""
-                     """ "tag": "wow","""
-                     """ "completiontime": null,""")
+        expected2 = (""" "id": 1,""")
         expected3 = (""" "foregroundhex": "#1111","""
                      """ "subtaskof": null,"""
                      """ "duedate": "10-2-2016T00:00Z","""
                      """ "name": "better task"}""")
         self.assertTrue(expected1 in r.content)
-        # self.assertTrue(expected2 in r.content)
+        self.assertTrue(expected2 in r.content)
         self.assertTrue(expected3 in r.content)
 
 
