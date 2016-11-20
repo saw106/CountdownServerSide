d69edb1a9e42cad9c0e82ef181036fad40b9e22f
Steven Wendling <saw106@case.edu>
2016-11-20 14:28:52 -0500

    more times were broke

--- setUpTestDB.py
+++ setUpTestDB.py
@@ -31,7 +31,7 @@ c.execute('''INSERT INTO tasks
                      "2016-10-1T00:00Z", "2016-10-26T00:00Z", "true",
                      "2016-10-26T00:00Z", NULL)''')
 c.execute('''INSERT INTO tasks
-             VALUES (12, "task_2", "description_2", "2016-11-15T12:30Z 12:30PM", "2", "tag_1", "FFFFFF", "BBBBBB",
+             VALUES (12, "task_2", "description_2", "2016-11-15T12:30Z", "2", "tag_1", "FFFFFF", "BBBBBB",
                      "2016-10-1T00:00Z", "2016-10-1T00:00Z", "false", null, NULL)''')
 c.execute('''INSERT INTO tasks
              VALUES (13, "task_3", "description_3", "2016-11-1T00:00Z", "1", "tag_2", "AAAAAA", "BBBBBB",
